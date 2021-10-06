e6f35ef67f34353eb3dcfd093200defb4ca36fc1
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-10-06 16:28:48 -0300

    endpoit de trocar a turma dos professores implementado

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app  from "./app"
+import changeTeachersTeam from "./endpoints/changeTeachersTeam"
 import createTeacher from "./endpoints/createTeacher"
 import createTeam from "./endpoints/createTeam"
 
 app.post('/teams', createTeam)
 app.post('/teachers', createTeacher)
+app.put('/teachers', changeTeachersTeam)
